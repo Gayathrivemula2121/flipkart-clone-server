654d91a7abd65c34795c794f197d5d42b02c1594
Gayathri Vemula <gayathrivemula2121@gmail.com>
2025-06-01 07:11:32 -0400

    Fix: use MONGO_URI from environment variable

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/flipkartclone', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
   console.log('Connected to MongoDB');
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
