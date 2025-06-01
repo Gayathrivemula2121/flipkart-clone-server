67765acefde69e07657a4fc64656121e24713b77
Gayathri Vemula <gayathrivemula2121@gmail.com>
2025-06-01 09:08:20 -0400

    Confirmed routes and models

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,8 +7,8 @@ router.get('/', async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
-  } catch (err) {
-    res.status(500).json({ message: 'Server error', error: err.message });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 });
 
